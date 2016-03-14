ff44a7c8077a83645f96d73e1e69a6d40e4d0937
Kevin Menard <kevin@nirvdrum.com>
2016-03-14 15:28:16 -0400

    Reworked the ERB benchmark to not eval.
    While ERB requires eval to truly run, the eval operation confounds the
    results for String performance. Instead, we now capture the code fragment
    that ERB would eval and run it in the benchmark harness.
    
    Since this operation is both reduced is scope and generally quite fast,
    I moved to using the microbenchmark harness so we can tweak the iteration
    count.

--- benchmarks/templating/erb.rb
+++ benchmarks/templating/erb.rb
@@ -23,24 +23,78 @@ require_relative 'context'
 
 TEMPLATE_SOURCE = File.read(File.join(File.dirname(__FILE__), 'view.erb'))
 
-def harness_input
-  ERB.new(TEMPLATE_SOURCE)
+def micro_harness_input
 end
 
-def harness_sample(input)
-  context = Context.new
-  context_binding = context.instance_eval { binding }
-
-  output = ''
-  100.times do
-    output = input.result(context_binding)
-  end
-
-  output
+def header
+  'Colors'
 end
 
-def harness_verify(output)
-  output.length == 1420
+def item
+  [ { name: 'red',   current: true,  url: '#red'   },
+    { name: 'green', current: false, url: '#green' },
+    { name: 'blue',  current: false, url: '#blue'  } ]
 end
 
-require 'bench9000/harness'
+def micro_harness_iterations
+  40_000
+end
+
+def micro_harness_sample(input)
+    _erbout = ''
+  ; Truffle::Primitive.convert_to_mutable_rope(_erbout) if ENV['USE_MUTABLE_ROPES'] && defined?(Truffle)
+  ; _erbout.concat "<!--\n"
+  ; _erbout.concat "~ Copyright (c) 2010 - 2015 Slim Team\n"
+  ; _erbout.concat "~\n"
+  ; _erbout.concat "~ Permission is hereby granted, free of charge, to any person obtaining a copy\n"
+  ; _erbout.concat "~ of this software and associated documentation files (the \"Software\"), to deal\n"
+  ; _erbout.concat "~ in the Software without restriction, including without limitation the rights\n"
+  ; _erbout.concat "~ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell\n"
+  ; _erbout.concat "~ copies of the Software, and to permit persons to whom the Software is\n"
+  ; _erbout.concat "~ furnished to do so, subject to the following conditions:\n"
+  ; _erbout.concat "~\n"
+  ; _erbout.concat "~ The above copyright notice and this permission notice shall be included in\n"
+  ; _erbout.concat "~ all copies or substantial portions of the Software.\n"
+  ; _erbout.concat "~\n"
+  ; _erbout.concat "~ THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\n"
+  ; _erbout.concat "~ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\n"
+  ; _erbout.concat "~ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\n"
+  ; _erbout.concat "~ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\n"
+  ; _erbout.concat "~ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\n"
+  ; _erbout.concat "~ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN\n"
+  ; _erbout.concat "~ THE SOFTWARE.\n"
+  ; _erbout.concat "-->\n"
+  ; _erbout.concat "\n"
+  ; _erbout.concat "<!DOCTYPE HTML>\n"
+  ; _erbout.concat "\n"
+  ; _erbout.concat "<html>\n"
+  ; _erbout.concat "<head>\n"
+  ; _erbout.concat "  <title>Simple Benchmark</title>\n"
+  ; _erbout.concat "</head>\n"
+  ; _erbout.concat "<body>\n"
+  ; _erbout.concat "<h1>"; _erbout.concat(( header ).to_s); _erbout.concat "</h1>\n"
+  ;  unless item.empty? ; _erbout.concat "\n"
+  ; _erbout.concat "  <ul>\n"
+  ; _erbout.concat "    ";  for i in item ; _erbout.concat "\n"
+  ; _erbout.concat "      ";  if i[:current] ; _erbout.concat "\n"
+  ; _erbout.concat "        <li><strong>"; _erbout.concat(( i[:name] ).to_s); _erbout.concat "</strong></li>\n"
+  ; _erbout.concat "      ";  else ; _erbout.concat "\n"
+  ; _erbout.concat "        <li><a href=\""; _erbout.concat(( i[:url] ).to_s); _erbout.concat "\">"; _erbout.concat(( i[:name] ).to_s); _erbout.concat "</a></li>\n"
+  ; _erbout.concat "      ";  end ; _erbout.concat "\n"
+  ; _erbout.concat "    ";  end ; _erbout.concat "\n"
+  ; _erbout.concat "  </ul>\n"
+  ;  else ; _erbout.concat "\n"
+  ; _erbout.concat "  <p>The list is empty.</p>\n"
+  ;  end ; _erbout.concat "\n"
+  ; _erbout.concat "</body>\n"
+  ; _erbout.concat "</html>\n"
+  ; _erbout.force_encoding(__ENCODING__)
+
+  _erbout.length
+end
+
+def micro_harness_expected
+  1420
+end
+
+require 'bench9000/micro-harness'
